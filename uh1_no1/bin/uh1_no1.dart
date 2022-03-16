8a0995ad94f2665a4cc374d6eb40c1d114f7726c
TerriusJeremiah <100283403+TerriusJeremiah@users.noreply.github.com>
2022-03-16 14:29:22 +0700

    Commit 2

--- uh1_no1/bin/uh1_no1.dart
+++ uh1_no1/bin/uh1_no1.dart
@@ -1,3 +1,18 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  stdout.write("Masukkan Angka: ");
+  var tmp = stdin.readLineSync();
+  int angka = int.parse(tmp!);
+
+  stdout.write("Hasil Faktor: ");
+
+  for (int i = 1; i <= angka; i++) {
+    if (angka % i == 0) {
+      stdout.write(i);
+    }
+    if (i != angka && angka % i == 0) {
+      stdout.write(', ');
+    }
+  }
 }
